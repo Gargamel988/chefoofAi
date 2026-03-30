5831b599f5fcec6101592e28cade73de8ca5d330
Gargamel988 <omeraydin1.web@gmail.com>
2026-03-30 17:30:57 +0300

    qas

--- hooks/useGetProfile.ts
+++ hooks/useGetProfile.ts
@@ -2,11 +2,15 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { getMyProfile } from "@/services/profiles";
 import { Profile } from "@/services/profiles";
 
-export const useGetProfile = () => {
+export const useGetProfile = (): Profile => {
   // Queries
   const { data: myProfile } = useSuspenseQuery<Profile>({
     queryKey: ["profile", "me"],
-    queryFn: getMyProfile,
+    queryFn: async () => {
+      const profile = await getMyProfile();
+      if (!profile) throw new Error("Profile not found");
+      return profile;
+    },
   });
 
   return myProfile;
