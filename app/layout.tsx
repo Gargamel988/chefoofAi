032104464456d206276c61dc95284542b02bb8da
Gargamel988 <omeraydin1.web@gmail.com>
2026-04-02 14:46:39 +0300

    asdaf

--- app/layout.tsx
+++ app/layout.tsx
@@ -153,6 +153,8 @@ export default async function RootLayout({
       <head>
         <JsonLd data={jsonLdApp} />
         <JsonLd data={jsonLdOrg} />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1444133443338193"
+          crossorigin="anonymous"></Script>
       </head>
 
       <body className={`${interFont.className} flex flex-col min-h-screen dark antialiased`}>
